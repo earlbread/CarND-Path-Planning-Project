5e43533c3ef784d17bedb25df018d01dba0e766b
Seunghun Lee <waydi1@gmail.com>
2017-10-05 16:52:56 +0900

    Slow down if other cars are too close

--- src/main.cpp
+++ src/main.cpp
@@ -242,7 +242,41 @@ int main() {
           	auto sensor_fusion = j[1]["sensor_fusion"];
             int prev_size = previous_path_x.size();
 
-            if (ref_vel < 49.5)
+            if (prev_size > 0)
+            {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            // find ref_v to use
+            for (int i = 0; i < sensor_fusion.size(); i++)
+            {
+              // car is in my lane
+              float d = sensor_fusion[i][6];
+              if (d < (2 + 4 * lane + 2) && d > (2 + 4 * lane - 2))
+              {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                // if using previous points can project s value out
+                check_car_s += ((double)prev_size * .02 * check_speed);
+
+                // check s values greater than mine and s gap
+                if ((check_car_s > car_s) && ((check_car_s - car_s) < 30))
+                {
+                  too_close = true;
+                }
+              }
+            }
+
+            if (too_close)
+            {
+              ref_vel -= 0.224;
+            }
+            else if (ref_vel < 49.5)
             {
               ref_vel += 0.224;
             }
